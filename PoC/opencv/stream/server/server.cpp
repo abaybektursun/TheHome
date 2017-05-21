7ad7efd21eee1374939cd4131076ad056331e16a
abaybektursun <abaybektursun@gmail.com>
2017-05-21 18:29:29 +0000

    server tcp

--- PoC/opencv/stream/server/server.cpp
+++ PoC/opencv/stream/server/server.cpp
@@ -1,24 +1,53 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <ctime>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-using namespace cv;
+
+#define MAX_CONNECTIONS 1
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+    int serverSocket,
+        doorSocket,
+        serverPort = 2654;
+    
+    struct sockaddr_in server_ip, client_ip;
+
+    pthread_t threads[MAX_CONNECTIONS];
+    
+    int ip_size = sizeof(struct sockaddr_in);
+    
+    // Create unbinded socket
+    serverSocket = socket(AF_INET , SOCK_STREAM , 0);
+    if (serverSocket == -1){
+        std::cerr << "Unable to create server socket\n";
+        exit(-1);
     }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
+    
+    //AF_UNIX    : UNIX domain protocols
+    //AF_INET    : Internet domain protocols
+    //SOCK_STREAM: TCP protocol
+    //SOCK_DGRAM : UDP protocol
+    //SOCK_RAW   : IP protocol
+
+    server_ip.sin_addr.s_addr = INADDR_ANY;
+    server_ip.sin_family = AF_INET;
+    server_ip.sin_port = htons(serverPort);
+    
+    //Assign a name (port, etc.) to the socket
+    if( bind(serverSocket,(struct sockaddr *)&server_ip , sizeof(server_ip)) < 0) {
+         perror("Failed to bind the socket");
+         exit(1);
     }
+    
+    // Start the server
+    listen(serverSocket , 3);
+    
+    
+    
     return 0;
 }
